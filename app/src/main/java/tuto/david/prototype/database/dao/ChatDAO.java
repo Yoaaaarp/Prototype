106439365ad0929820200755bb9db8fcae9e5071
David Villa <david.villa@heig-vd.ch>
2016-05-30 12:03:39 +0200

    Completion ChatDAO

--- app/src/main/java/tuto/david/prototype/database/dao/ChatDAO.java
+++ app/src/main/java/tuto/david/prototype/database/dao/ChatDAO.java
@@ -1,7 +1,39 @@
 package tuto.david.prototype.database.dao;
 
-/**
- * Created by David on 30.05.2016.
- */
-public class ChatDAO {
+import android.content.Context;
+
+import tuto.david.prototype.database.entity.Chat;
+
+
+public class ChatDAO extends DAOBase {
+    public static final String CHAT_KEY = "id";
+    public static final String CHAT_TITLE = "title";
+
+    public static final String CHAT_TABLE_NAME = "chat";
+    public static final String CHAT_TABLE_CREATE = "CREATE TABLE " + CHAT_TABLE_NAME
+            + " (" + CHAT_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + CHAT_TITLE + " TEXT NOT NULL);";
+    public static final String CHAT_TABLE_DROP = "DROP TABLE IF EXISTS " + CHAT_TABLE_NAME + ";";
+
+
+    public ChatDAO(Context context) {
+        super(context);
+    }
+
+    public void create(Chat c){
+        //TODO
+    }
+
+    public void delete(long id){
+        //TODO
+    }
+
+    public void update(Chat c){
+        //TODO
+    }
+
+    public Chat read(long id){
+        //TODO
+        return null;
+    }
 }
