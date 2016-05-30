2346db539241a700895de317681369c848822098
David Villa <david.villa@heig-vd.ch>
2016-05-30 10:45:31 +0200

    Completion entité Chat

--- app/src/main/java/tuto/david/prototype/database/entity/Chat.java
+++ app/src/main/java/tuto/david/prototype/database/entity/Chat.java
@@ -1,6 +1,7 @@
 package tuto.david.prototype.database.entity;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Chat {
@@ -9,5 +10,51 @@ public class Chat {
     private List<Member> members;
     private List<Message> messages;
 
+    // constructeur pour nouveau fil de discussion
+    public Chat(long id, String title, List<Member> members){
+        this.id = id;
+        this.title = title;
+        this.members = members;
+        messages = new ArrayList<>();
+    }
 
+    // constructeur pour fil de discussion existant (après recupépration des données depuis le serveur
+    public Chat(long id, String title, List<Member> members, List<Message> messages){
+        this.id = id;
+        this.title = title;
+        this.members = members;
+        this.messages = messages;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
 }
