010f6c121e6e524b1739c259d0c549c339947f17
David Villa <david.villa@heig-vd.ch>
2016-05-30 10:50:34 +0200

    Completion de l'entite Message

--- app/src/main/java/tuto/david/prototype/database/entity/Message.java
+++ app/src/main/java/tuto/david/prototype/database/entity/Message.java
@@ -1,5 +1,50 @@
 package tuto.david.prototype.database.entity;
 
 
+import java.util.Date;
+
 public class Message {
+    private Member author;
+    private Chat chat;
+    private Date creationDate;
+    private int importance;
+
+    public Message(Member author, Chat chat, Date creationDate, int importance){
+        this.author = author;
+        this.chat = chat;
+        this.creationDate = creationDate;
+        this.importance = importance;
+    }
+
+    public Member getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Member author) {
+        this.author = author;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getImportance() {
+        return importance;
+    }
+
+    public void setImportance(int importance) {
+        this.importance = importance;
+    }
 }
